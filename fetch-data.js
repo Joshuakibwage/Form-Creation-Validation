f5ae0107ea88aed73ef0768dc693011b63d43c1a
Josh <joshuakibwage23@gmail.com>
2024-10-12 10:12:07 +0300

    Add fetching data using try catch

--- fetch-data.js
+++ fetch-data.js
@@ -2,5 +2,16 @@
 async function fetchUserData() {
     const apiUrl = 'https://jsonplaceholder.typicode.com/users';
     const dataContainer = document.getElementById('api-data');
-}
+
+    try{
+        const response = await fetch(apiUrl);
+        if(!response.ok) {
+            throw new Error(`response status: ${response.status}`);
+        }
+        const users = await response.json();
+        console.log(json);
+    } catch(error) {
+        console.error(error.message);
+    }
+};
 
